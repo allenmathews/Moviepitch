64ff9fb8a65d69c2f0db5eab3d7cd7c44e23fd05
Allen Mathews <allenjoemathews@gmail.com>
2023-12-30 21:19:40 +1100

    ad

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { process } from '/env'
 import { Configuration, OpenAIApi } from 'openai'
 
+
 const setupTextarea = document.getElementById('setup-textarea')
 const setupInputContainer = document.getElementById('setup-input-container')
 const movieBossText = document.getElementById('movie-boss-text')
@@ -12,20 +13,18 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 document.getElementById("send-btn").addEventListener("click", () => {
-    if (setupTextarea.value) {
-        const userInput = setupTextarea.value
-        setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
-        movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
-        fetchBotReply(userInput)
-    }
+    // if (setupTextarea.value) {
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
+        // }
+    fetchBotReply()
 })
 
-async function fetchBotReply(outline) {
+async function fetchBotReply() {
     const response = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `Generate a short message to enthusiastically say "${outline}" sounds interesting and that you need some minutes to think about it. Mention one aspect of the sentence."`,
-        max_tokens: 60
+        'model': 'text-davinci-003',
+        'prompt': 'Sound enthusiastic in five words or less.'
     })
     movieBossText.innerText = response.data.choices[0].text.trim()
-    console.log(response)
 }
+// [{text: " Excited, Enthusiastic, Driven!",
