d8991dce77e4457a847a9331f131b395f74d4e7d
Allen Mathews <allenjoemathews@gmail.com>
2024-01-07 20:50:13 +1100

    Ad

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function fetchBotReply(outline) {
     outline: Two dogs fall in love and move to Hawaii to learn to surf.
     message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
     ###
-    outline: A plane crashes in the jungle and the passengers have to walk 1000km to safety.
+    outline:A plane crashes in the jungle and the passengers have to walk 1000km to safety.
     message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
     ###
     outline: A group of corrupt lawyers try to send an innocent woman to jail.
@@ -47,15 +47,28 @@ async function fetchBotReply(outline) {
 async function fetchSynopsis(outline) {
     const response = await openai.createCompletion({
         model: 'text-davinci-003',
-        prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline
+        prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline.
     ###
     outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
-    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.  
     ###
     outline: ${outline}
     synopsis: 
     `,
         max_tokens: 700
     })
-    document.getElementById('output-text').innerText = response.data.choices[0].text.trim()
+    const synopsis = response.data.choices[0].text.trim()
+    document.getElementById('output-text').innerText = synopsis
+    fetchTitle(synopsis)
+}
+
+async function fetchTitle(synopsis) {
+    const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `Generate a catchy movie title for this synopsis: ${synopsis}`,
+        max_tokens: 25,
+        temperature: 0.7
+    })
+
+    document.getElementById('output-title').innerText = response.data.choices[0].text.trim()
 }
